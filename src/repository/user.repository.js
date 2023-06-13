eb6b3bc8012689e8872dc229c8332e6fd1a58601
honeymirara <honeymirara@gmail.com>
2023-06-13 12:16:46 +0300

    bcrypt

--- src/repository/user.repository.js
+++ src/repository/user.repository.js
@@ -1,10 +1,18 @@
 const pool = require('../db');
 
-async function createUserDB(name, surname, email, pwd){
-    const client = pool.connect();
+async function createUserDB(name, surname, email, pwd) {
+    const client = await pool.connect();
     const sql = 'INSERT INTO users(name, surname, email, pwd) VALUES ($1, $2, $3, $4) RETURNING*';
-    const result = (await ((await client).query(sql, [name, surname, email, pwd]))).rows;
+    const result = (await client.query(sql, [name, surname, email, pwd])).rows;
     return result;
 };
 
-module.exports = {createUserDB};
+
+async function getUserByEmailDB(email) {
+    const client = await pool.connect();
+    const sql = 'SELECT * FROM users WHERE email = $1'
+    const result = (await client.query(sql, [email])).rows;
+    return result;
+}
+
+module.exports = { createUserDB, getUserByEmailDB };
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,10 +1,20 @@
-const {createUserDB} = require('../repository/user.repository');
+const {createUserDB, getUserByEmailDB} = require('../repository/user.repository');
+const bcrypt = require('bcrypt');
+
 
 async function createUser(name, surname, email, pwd){
-    const data = await createUserDB(name, surname, email, pwd);
+    const foundUser = await getUserByEmailDB(email);
+    if (foundUser.length) throw new Error('this user already exists');
+
+    const salt = 3;
+    const hashPassword = await bcrypt.hash(pwd, salt);
+
+    const data = await createUserDB(name, surname, email, hashPassword);
     if(!data.length) throw new Error('user is not found');
     return data;
 };
+
+
 
 
 module.exports = {createUser};
